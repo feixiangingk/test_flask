427abda86113108b3caec68b62df54af624a2098
feixiangingk <feixiangingk@163.com>
2018-03-28 00:57:05 +0800

    update models

--- app/models.py
+++ app/models.py
@@ -4,13 +4,22 @@
 # IDE:         PyCharm
 # anthor:      ZT@gufan
 
-from app import create_app,db
+from . import create_app,db
 
 class Role(db.Model):
     __tablename__="roles"
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(64),nullable=True,unique=False)
     users=db.relationship("User",backref="role")
+
+    #数据初始化 可以Python shell中执行:
+    #from app import db
+    #from app.models import Role
+    #Role.seed()
+    @staticmethod
+    def seed():
+        db.session.add_all(map(lambda r:Role(name=r),["Guests","Administrators"]))
+        db.session.commit()
 
     def __repr__(self):
         return "<Role {}>".format(self.name)
@@ -24,7 +33,15 @@ class User(db.Model):
     role_id=db.Column(db.Integer,db.ForeignKey("roles.id"))
 
     def __repr__(self):
-        return "User {}".format(self.username)
+        return "User {}".format(self.name)
+
+    @staticmethod
+    def on_created(target, value,oldvalue, initiator):
+        target.role_id=Role.query.filter_by(name="Guests").first().id
+
+#SQLAlchemy触发器功能  http://docs.sqlalchemy.org/en/latest/orm/events.html
+db.event.listen(User.name,'set',User.on_created)
+
 
 def insert_Role():
     admin_role=None
